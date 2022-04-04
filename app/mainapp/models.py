4cfa9d738e06b00fad409ae75b2ee03843c654a1
Arjun Krishna <arjunkrishna1306@gmail.com>
2022-04-04 18:31:45 -0400

    add models for org, vend, stak

--- app/mainapp/models.py
+++ app/mainapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class organization(models.Model):
+    name = models.CharField(max_length=100)
+    support_email = models.EmailField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " (org)"
+
+class vendor(models.Model):
+    name = models.CharField(max_length=100)
+    dpa_url = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " (vend)"
+
+class stakeholder(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " (vend)"
