fcd4ce39389b6c669d248235c36a6b23cb72f119
Arjun Krishna <arjunkrishna1306@gmail.com>
2022-04-26 16:26:22 -0400

    automatically pull and use latest docker image

--- main.tf
+++ main.tf
@@ -19,5 +19,18 @@ resource "aws_instance" "dpalist_vm" {
   instance_type          = "t2.micro"
   key_name               = "dpalist1"
   vpc_security_group_ids = ["sg-0433c6a8b20f96b18"]
+
+  user_data = <<EOF
+#!/bin/bash
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install -y docker-ce
+sudo chmod 666 /var/run/docker.sock
+docker pull zubgkaow29cuto/dpalist
+docker run -d -p 3000:3000 zubgkaow29cuto/dpalist
+  EOF
+
   tags                   = { "Name" : "dpalist_vm" }
 }
